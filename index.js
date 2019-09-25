8ce882557801039eb6fa15cf9e4cd246ea85faa0
nestorlora <nestorlora@geeklab.es>
2019-09-26 00:15:35 +0200

    :globe_with_meridians: :speech_balloon: :card_file_box: :bulb: Implementing #2

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const cors = require('cors');
 const app = express();
 const config = require('./config.json');
 const db = require('./db');
-app.listen(config.port, () => console.log('RUTPAM-Server v'+config.version+' escuchando en el puerto '+config.port));
+app.listen(config.port, () => console.log('RUTPAM-Server v'+config.version+' listening on port '+config.port));
 app.use(cors());
 
 app.get('/info', function(req, res){
     let info = {
         'version': config.version,
-        'contacto': config.admin,
+        'contact': config.admin,
         'web': config.web,
-        'mantenimiento': config.maintenance
+        'maintenance': config.maintenance
     };
     res.status(200).json(info).end();
 });
@@ -20,12 +20,12 @@ app.get('/info', function(req, res){
 app.get('/tipos_subida_bajada', function(req, res){
     if(req.query.id){
         var id = Number(req.query.id);
-        db.query('SELECT * FROM `tipos_subida_bajada` WHERE `id` = '+id, function(err, rows, fields){
+        db.query('SELECT * FROM `pickup_dropoff_types` WHERE `id` = '+id, function(err, rows, fields){
             if(err) throw err;
             res.status(200).json(rows).end();
         });
     }else{
-        db.query('SELECT * FROM `tipos_subida_bajada`', function(err, rows, fields){
+        db.query('SELECT * FROM `pickup_dropoff_types`', function(err, rows, fields){
             if(err) throw err;
             res.status(200).json(rows).end();
         });
