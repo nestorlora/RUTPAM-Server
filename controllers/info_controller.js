869306fc540939da98dc8112e6f0304cad3a1883
nestorlora <nestorlora@geeklab.es>
2019-12-22 18:09:33 +0100

    :bulb: Documenting files

--- controllers/info_controller.js
+++ controllers/info_controller.js
@@ -1,3 +1,11 @@
+/**
+ * @file Controller functions for /info
+ * @version v1.0
+ * @author Néstor Manuel Lora Romero <nestorlora@geeklab.es>
+ * @copyright Geeklab - Néstor Manuel Lora Romero 2018-2019
+ * @license MIT
+ */
+
 config = require("../config/config.json");
 
 exports.getInfo = function(req, res){
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,11 @@
+/**
+ * @file App main router
+ * @version v1.0
+ * @author Néstor Manuel Lora Romero <nestorlora@geeklab.es>
+ * @copyright Geeklab - Néstor Manuel Lora Romero 2018-2019
+ * @license MIT
+ */
+
 module.exports = function(app) {
     const info = require("../controllers/info_controller");
     
