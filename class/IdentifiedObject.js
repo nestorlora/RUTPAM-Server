781cfb16f228ce5f839c76bd8d36b444fd3e3083
nestorlora <nestorlora@geeklab.es>
2019-12-23 14:51:51 +0100

    :recycle: :fire: Rewriting IdentifiedObject
    Now adapted to work with /data/ObjectAdapter design

--- class/IdentifiedObject.js
+++ class/IdentifiedObject.js
@@ -1,6 +1,6 @@
 /**
  * @file Class IdentifiedObject definition
- * @version v1.1
+ * @version v2.0
  * @author Néstor Manuel Lora Romero <nestorlora@geeklab.es>
  * @copyright Geeklab - Néstor Manuel Lora Romero 2018-2019
  * @license MIT
@@ -9,27 +9,11 @@
 class IdentifiedObject extends Object {
     constructor(obj){
         super();
-        if(obj._id){
-            this._id = obj._id;
-        }else if(obj.id){
-            this._id = obj.id;
+        if(obj.id){
+            this.id = obj.id;
         }else{
-            this._id = null;
+            this.id = null;
         }
-    }
-    get id(){
-        return this._id;
-    }
-
-    set id(val){
-        this._id = val;
-        return this._id;
-    }
-    export(){
-        let copy = Object.assign({}, this);
-        copy.id = copy._id;
-        delete copy._id;
-        return copy;
     }
 }
 
