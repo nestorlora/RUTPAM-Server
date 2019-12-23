8dbbc8ae88fdb38a970b3f25ecfb3fab4e3e1814
nestorlora <nestorlora@geeklab.es>
2019-12-23 14:45:43 +0100

    :pencil2: Typos

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@
  */
 
 module.exports = function(app) {
-    const info = require("../controllers/info_controller");
+    const info = require('../controllers/info_controller');
     
     app.route('/info')
         .get(info.getInfo);
