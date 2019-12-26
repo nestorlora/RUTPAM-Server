0baba5f4dc2d7d6df24bf039881f858d836b1252
nestorlora <nestorlora@geeklab.es>
2019-12-27 00:48:43 +0100

    :bulb: Documenting

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ * @file Express app main file
+ * @version v1.4
+ * @author Néstor Manuel Lora Romero <nestorlora@geeklab.es>
+ * @copyright Geeklab - Néstor Manuel Lora Romero 2018-2019
+ * @license MIT
+ */
+
 'use strict'
 // Library imports
 const express = require('express');
