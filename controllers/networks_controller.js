6f7370bc61cdf2c038a7d21b05e8cc340bd113d2
nestorlora <nestorlora@geeklab.es>
2019-12-27 00:51:45 +0100

    :sparkles: Added editNetwork()

--- controllers/networks_controller.js
+++ controllers/networks_controller.js
@@ -1,6 +1,6 @@
 /**
  * @file Controller functions for /networks
- * @version v1.2
+ * @version v1.3
  * @author Néstor Manuel Lora Romero <nestorlora@geeklab.es>
  * @copyright Geeklab - Néstor Manuel Lora Romero 2018-2019
  * @license MIT
@@ -18,6 +18,13 @@ exports.getAllNetworks = function(req, res){
 exports.getNetwork = function(req, res){
     let id = req.params.id;
     data.getById('networks', id, Network, (data, err)=>{
+        callback(res, data, err);
+    });
+}
+
+exports.editNetwork = function(req, res){
+    let net = req.body;
+    data.update('networks', net, Network, (data, err)=>{
         callback(res, data, err);
     });
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 /**
  * @file App main router
- * @version v1.3
+ * @version v1.4
  * @author Néstor Manuel Lora Romero <nestorlora@geeklab.es>
  * @copyright Geeklab - Néstor Manuel Lora Romero 2018-2019
  * @license MIT
@@ -13,7 +13,8 @@ module.exports = function(app) {
     app.route('/info')
         .get(info.getInfo);
     app.route('/networks')
-        .get(networks.getAllNetworks);
+        .get(networks.getAllNetworks)
+        .patch(networks.editNetwork);
     app.route('/networks/:id')
         .get(networks.getNetwork);
     app.route('/*')
