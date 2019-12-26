30e27d2e83d0241dec625d941a9820da636dd917
nestorlora <nestorlora@geeklab.es>
2019-12-27 00:49:56 +0100

    :sparkles: Added request.body parsing to JSON

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * @file Express app main file
- * @version v1.4
+ * @version v1.5
  * @author Néstor Manuel Lora Romero <nestorlora@geeklab.es>
  * @copyright Geeklab - Néstor Manuel Lora Romero 2018-2019
  * @license MIT
@@ -16,6 +16,7 @@ const config = require('./config/config.json');
 // Express app
 const app = express();
 app.listen(config.port, () => console.log('RUTPAM-Server v'+config.version+' listening on port '+config.port));
+app.use(express.json());
 app.use(cors());
 var routes = require("./routes/index");
 routes(app);
