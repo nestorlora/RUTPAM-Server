5bf4a6cfa7976c4321b1ab0a3800636953722789
nestorlora <nestorlora@geeklab.es>
2019-12-29 13:28:05 +0100

    :pencil2: Typo
    Past 2 commits must have increased version counter

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 /**
  * @file App main router
- * @version v1.5
+ * @version v1.7
  * @author Néstor Manuel Lora Romero <nestorlora@geeklab.es>
  * @copyright Geeklab - Néstor Manuel Lora Romero 2018-2019
  * @license MIT
