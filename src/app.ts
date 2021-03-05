54ac52b180c456a15da62931c6972fe4fe4b5028
nestorlora <nestorlora@geeklab.es>
2021-03-05 12:32:58 +0100

    Add Typescript
    Signed-off-by: nestorlora <nestorlora@geeklab.es>

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+/**
+ * @file Express server main file
+ * @version v1.8.2
+ * @author Néstor Manuel Lora Romero <nestorlora@geeklab.es>
+ * @copyright Geeklab - Néstor Manuel Lora Romero 2018-2020
+ * @license MIT
+ */
+
+'use strict'
+// Library imports
+import express from 'express';
+import cors from 'cors';
